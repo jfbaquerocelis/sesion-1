6821625d126d1dd9af54c1761168f327ab34301c
John Baquero Celis <jfbaquerocelis@gmail.com>
2019-06-20 09:37:20 -0500

    Functions

--- index.js
+++ index.js
@@ -1,5 +1,20 @@
 'use strict'
 
-var user = 'Liana'
+// Synchronous
+function suma (a, b) {
+  return a + b
+}
 
-console.log(`Hello ${user}`)
+let result = suma(3, 7)
+console.log(result)
+
+// Asynchronous
+function sumaAsync (a, b, callback) {
+  let result = a + b
+
+  callback(result)
+}
+
+sumaAsync(5, 9, function (result) {
+  console.log(result)
+})
